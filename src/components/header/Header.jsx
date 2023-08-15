2f43bab7d1c04fda3572c782427a1713f1e01773
brucarlota <bru.carlota@yahoo.com.br>
2023-08-15 18:35:25 -0300

    fix images

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,13 +6,13 @@ const Header = () => {
   return (
     <HeaderWrapper className="header">
       <div className="icon">
-        <img className="header__logo" src="src/assets/iconoir_book.png"></img>
+        <img className="header__logo" src="/iconoir_book.png"></img>
       </div>
       <ControlsWrapper className="header__controls">
         <Autocomplete id="header__font"></Autocomplete>
         <Divider className="header__divider" orientation="vertical" variant="middle" flexItem />
         <Switch id="header__themeSelector"></Switch>
-        <img className="header__image-moon" src="src/assets/iconoir_half-moon.png"></img>
+        <img className="header__image-moon" src="/iconoir_half-moon.png"></img>
       </ControlsWrapper>
     </HeaderWrapper>
   );
